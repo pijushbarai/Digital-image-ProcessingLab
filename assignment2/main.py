a4c9ac4861f98efe2651746d4aba78d130e8e535
pijushbarai <pijushbarai1@gmail.com>
2022-06-09 16:49:48 +0600

    assignment 2 is preparing

---
+++ assignment2/main.py
@@ -0,0 +1,14 @@
+from cv2 import imread
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+
+def main():
+    img_path = r'E:\Digital-image-ProcessingLab\assignment2\images\pigeon.jpg'
+    img = plt.imread(img_path)
+    grayscale = cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
+    
+    print(img,img.max(),img.min(),img.shape)
+    plt.imshow(grayscale,cmap='gray')
+    plt.show()
+main()
