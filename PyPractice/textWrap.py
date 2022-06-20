2759af771d65e12d94620f19b381e1d0aa23d20e
pijushbarai <pijushbarai1@gmail.com>
2022-06-21 02:07:23 +0600

    solved textwrap problem

--- PyPractice/textWrap.py
+++ PyPractice/textWrap.py
@@ -1,15 +1,18 @@
+from re import sub
 import textwrap
 
 def wrap(string, max_width):
     list = []
+    str = ''
     length = len(string)
     for i in range(0,length,max_width):
         if(i+max_width > length-1):
             subString = string[i:length]
         else:
             subString = string[i:i+max_width]
-        list.append(subString)
-        return 
+        subString  = subString + '\n'
+        str += subString
+    return str
 if __name__ == '__main__':
     string, max_width = input(), int(input())
     result = wrap(string, max_width)
