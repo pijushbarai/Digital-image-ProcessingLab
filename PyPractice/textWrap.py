5a0a18d10c9509cc7b5f9feb50651b6f4c570eb2
pijushbarai <pijushbarai1@gmail.com>
2022-06-21 01:57:26 +0600

    move toward solution

--- PyPractice/textWrap.py
+++ PyPractice/textWrap.py
@@ -1,10 +1,15 @@
 import textwrap
 
 def wrap(string, max_width):
-    x = 'sahfkas\nfakdfja\naksdhfa'
-    print(x)
-    return 
-
+    list = []
+    length = len(string)
+    for i in range(0,length,max_width):
+        if(i+max_width > length-1):
+            subString = string[i:length]
+        else:
+            subString = string[i:i+max_width]
+        list.append(subString)
+        return 
 if __name__ == '__main__':
     string, max_width = input(), int(input())
     result = wrap(string, max_width)
