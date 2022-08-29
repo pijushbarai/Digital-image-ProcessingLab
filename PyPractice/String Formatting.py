a601e91c972fb9ea3d8803f802620710a6d71aa9
pijushbarai <pijushbarai1@gmail.com>
2022-08-29 09:34:29 +0600

    done

--- PyPractice/String Formatting.py
+++ PyPractice/String Formatting.py
@@ -1,17 +1,15 @@
 
-
-
-def decimalToBinary(n):
-    return bin(n).replace("0b", "")
-def decimalToHex(n):
-    return hex(n).replace("0x","")
-def decimalToOctal(n):
-    return oct(n).replace("0o","")
+from turtle import width
 
 
 def main():
     n = int(input())
-    for i in range(1,n+1):
-        print(i," ",decimalToOctal(i)," " , decimalToHex(i)," ",decimalToBinary(i))
+    # # width = len("{0:b}")
+    # for i in range(1,n+1):
+    #     print("{0:{w}d} {0:{w}o} {0:{w}x} {0:{w}b}".format(i, w = len("{0:b}".format(n))))
+    
+    width = len("{0:b}".format(n))
+    for i in range(1, n + 1):
+        print("{0:{w}d} {0:{w}o} {0:{w}X} {0:{w}b}".format(i, w = width))
 
 main()
